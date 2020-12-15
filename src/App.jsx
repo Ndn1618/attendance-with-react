1d4c0034b6f80d0c73fe53e9c7309b915b731316
Ndn1618 <ndilbar1618@gmail.com>
2020-12-15 19:54:36 +0500

    Add users full name to new options

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from 'react'
+import Data, {data} from './Data'
 import './App.css'
 
 function App () {
+  const [userInformations] = useState([...Data])
+  const [userAttendanceInfos] = useState([...data])
 
+  return (
+    <select class="form-control w-75 mx-auto bg-dark text-light border-0 shadow-none mb-4" name="username"
+        id="usernameSelect" defaultValue='DEFAULT' required>
+        <option value="DEFAULT" disabled>Select student by full name</option>
+        return (
+          {
+            userInformations.map(userInformation =>
+              <option value={userInformation.id} key={userInformation.id}>
+                {userInformation.name} {userInformation.surname}
+              </option>
+            )
+          }
+        )
+    </select>
+  )
 
 }
 
