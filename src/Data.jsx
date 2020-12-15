ceb75dee50b42bce4ee5fd5f2a30ff8c191923cb
Ndn1618 <ndilbar1618@gmail.com>
2020-12-15 19:44:03 +0500

    Add Data of Users

---
+++ src/Data.jsx
@@ -0,0 +1,127 @@
+const Data = [
+  {
+    id: 0,
+    name: 'Muhammadxon',
+    surname: 'Najimov',
+  },
+  {
+    id: 1,
+    name: 'Malika',
+    surname: 'Baxtiyorova',
+  },
+  {
+    id: 2,
+    name: 'Dilbar',
+    surname: 'Norqobulova',
+  }
+];
+
+export const data = [
+  {
+    id: 1,
+    userId: 1,
+    date: '12/09/2020',
+    visit: 'OK',
+    score: 10,
+  },
+  {
+    id: 2,
+    userId: 1,
+    date: '12/08/2020',
+    visit: 'OK',
+    score: 9,
+  },
+  {
+    id: 3,
+    userId: 1,
+    date: '12/07/2020',
+    visit: 'OK',
+    score: 10,
+  },
+  {
+    id: 4,
+    userId: 1,
+    date: '12/06/2020',
+    visit: '-',
+    score: 0,
+  },
+  {
+    id: 5,
+    userId: 1,
+    date: '12/05/2020',
+    visit: '-',
+    score: 0,
+  },
+  {
+    id: 6,
+    userId: 2,
+    date: '12/09/2020',
+    visit: 'OK',
+    score: 8,
+  },
+  {
+    id: 7,
+    userId: 2,
+    date: '12/08/2020',
+    visit: 'OK',
+    score: 8,
+  },
+  {
+    id: 8,
+    userId: 2,
+    date: '12/07/2020',
+    visit: 'OK',
+    score: 8,
+  },
+  {
+    id: 9,
+    userId: 2,
+    date: '12/06/2020',
+    visit: '-',
+    score: 0,
+  },
+  {
+    id: 10,
+    userId: 2,
+    date: '12/05/2020',
+    visit: '-',
+    score: 0,
+  },
+  {
+    id: 11,
+    userId: 0,
+    date: '12/09/2020',
+    visit: 'OK',
+    score: 10,
+  },
+  {
+    id: 12,
+    userId: 0,
+    date: '12/08/2020',
+    visit: 'OK',
+    score: 10,
+  },
+  {
+    id: 13,
+    userId: 0,
+    date: '12/07/2020',
+    visit: 'OK',
+    score: 10,
+  },
+  {
+    id: 14,
+    userId: 0,
+    date: '12/06/2020',
+    visit: '-',
+    score: 10,
+  },
+  {
+    id: 15,
+    userId: 0,
+    date: '12/05/2020',
+    visit: 'OK',
+    score: 10,
+  },
+]
+
+export default Data
