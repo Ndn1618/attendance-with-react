05d24df223d5d1bc128b36be3deddb6adae89b5a
Ndn1618 <ndilbar1618@gmail.com>
2020-12-15 20:18:31 +0500

    Show users attendances when selected user by full name

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,55 @@ import './App.css'
 function App () {
   const [userInformations] = useState([...Data])
   const [userAttendanceInfos] = useState([...data])
+  const [userAttendances, setUserAttendances] = useState([])
+
+  const showUserAttendances = (evt => {
+    setUserAttendances(userAttendanceInfos.filter(userAttendanceInfo => userAttendanceInfo.userId === Number(evt.target.value)))
+  })
 
   return (
-    <select class="form-control w-75 mx-auto bg-dark text-light border-0 shadow-none mb-4" name="username"
-        id="usernameSelect" defaultValue='DEFAULT' required>
-        <option value="DEFAULT" disabled>Select student by full name</option>
-        return (
-          {
-            userInformations.map(userInformation =>
-              <option value={userInformation.id} key={userInformation.id}>
-                {userInformation.name} {userInformation.surname}
-              </option>
+    <>
+    <select className="form-control w-75 mx-auto bg-dark text-light border-0 shadow-none mb-4" name="username"
+    id="usernameSelect" defaultValue='DEFAULT' onChange={showUserAttendances.bind(null)} required>
+    <option value="DEFAULT" disabled>Select student by full name</option>
+      {
+        userInformations.map(userInformation => {
+          return (
+            <option value={userInformation.id} key={userInformation.id}>
+            {userInformation.name} {userInformation.surname}
+            </option>
+          )
+        }
+        )
+      }
+    </select>
+
+    <table className="table w-75 mx-auto table-dark rounded overflow-hidden">
+      <thead>
+        <tr>
+        <th scope="col">Date</th>
+        <th scope="col">Visit</th>
+        <th scope="col">Score</th>
+        </tr>
+      </thead>
+      <tbody id="userInfoTableBody">
+        {
+          userAttendances.map(userAttendance => {
+
+            return (
+              <tr key={userAttendance.id}>
+                <td>{userAttendance.date}</td>
+                <td>{userAttendance.visit ? 'OK' : '-'}</td>
+                <td>{userAttendance.score}</td>
+              </tr>
             )
           }
-        )
-    </select>
+          )
+        }
+      </tbody>
+    </table>
+  </>
   )
-
 }
 
 export default App
